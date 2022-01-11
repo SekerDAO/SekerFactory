1fa0b45c64e0abd0630d04ca5174c7f6947f6200
Kirill Klimenko <klimenkomud@gmail.com>
2022-01-11 03:42:41 +0200

    Update HTML template, fix Input, add Subscribe section

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,49 +1,18 @@
 import {FunctionComponent} from "react"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import Button from "../components/Button"
 import EventDetails from "../components/EventDetails"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import HomePage from "../components/Home"
-import ImagePlaceholder from "../components/ImagePlaceholder"
-import {FEATURED_EVENT} from "../data/events"
-import "./index.scss"
 
 const App: FunctionComponent = () => (
 	<Router>
-		<div className="app">
-			<Header>
-				<div className="app__header-featured-event">
-					<div className="app__header-featured-event-col">
-						{FEATURED_EVENT.bannerSrc ? (
-							<img src={FEATURED_EVENT.bannerSrc} alt={FEATURED_EVENT.title} />
-						) : (
-							<ImagePlaceholder />
-						)}
-					</div>
-					<div className="app__header-featured-event-col">
-						<h1>
-							{FEATURED_EVENT.title} <br /> {FEATURED_EVENT.dateReadable}
-						</h1>
-						<p>Hosted by:</p>
-						<h2>
-							<p>Seker Factory 001</p>
-							<p>836 S Los Angeles Street</p>
-							<p>Los Angeles, CA 90014</p>
-						</h2>
-						<p className="app__header-featured-event-col-description">
-							{FEATURED_EVENT.description}
-						</p>
-						<Button>RSVP</Button>
-					</div>
-				</div>
-			</Header>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/events/:eventSlug" element={<EventDetails />} />
-			</Routes>
-			<Footer />
-		</div>
+		<Header />
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/events/:eventSlug" element={<EventDetails />} />
+		</Routes>
+		<Footer />
 	</Router>
 )
 
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,26 +1,58 @@
 import {FunctionComponent} from "react"
-import {UPCOMING_EVENTS} from "../../data/events"
+import {UPCOMING_EVENTS, FEATURED_EVENT} from "../../data/events"
 import Button from "../Button"
+import ImagePlaceholder from "../ImagePlaceholder"
+import Input from "../Input"
+import "./index.scss"
 
 const HomePage: FunctionComponent = () => (
-	<main>
+	<main className="home-page">
+		<section className="featured-event">
+			<div className="featured-event__col">
+				{FEATURED_EVENT.bannerSrc ? (
+					<img src={FEATURED_EVENT.bannerSrc} alt={FEATURED_EVENT.title} />
+				) : (
+					<ImagePlaceholder />
+				)}
+			</div>
+			<div className="featured-event__col">
+				<h1>
+					{FEATURED_EVENT.title} <br /> {FEATURED_EVENT.dateReadable}
+				</h1>
+				<p>Hosted by:</p>
+				<h2>
+					<p>Seker Factory 001</p>
+					<p>836 S Los Angeles Street</p>
+					<p>Los Angeles, CA 90014</p>
+				</h2>
+				<p className="featured-event__col-description">{FEATURED_EVENT.description}</p>
+				<Button>RSVP</Button>
+			</div>
+		</section>
+		<section className="subscribe">
+			<h1>Keep in Touch for Future Events</h1>
+			<div className="subscribe__input-container">
+				<Input placeholder="Enter your email" borders="bottom" />
+				<Button variant="secondary">Join Now</Button>
+			</div>
+		</section>
 		<section className="upcoming-events">
-			<div className="upcoming-events-header">
+			<div className="upcoming-events__header">
 				<h2>Upcoming Events</h2>
 				<Button variant="link">Previous Events</Button>
 			</div>
 		</section>
-		<div className="app__footer-upcoming-events-list">
+		<section className="upcoming-events__list">
 			{UPCOMING_EVENTS.map(eventContent => (
-				<div className="app__footer-upcoming-events-list-item" key={eventContent.slug}>
+				<div className="upcoming-events__list-item" key={eventContent.slug}>
 					<img
 						alt={eventContent.title}
 						src={eventContent.bannerSrc}
-						className="app__footer-upcoming-events-list-item-banner"
+						className="upcoming-events__list-item-banner"
 					/>
 				</div>
 			))}
-		</div>
+		</section>
 	</main>
 )
 
