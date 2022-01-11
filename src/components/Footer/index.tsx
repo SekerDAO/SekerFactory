4679b2b7de8b917847f80e9f3a37fd52d85a0700
Kirill Klimenko <klimenkomud@gmail.com>
2022-01-11 22:59:23 +0200

    Finalize content for home page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,28 @@
-import {FunctionComponent} from "react"
+import {FunctionComponent, useState} from "react"
+import Stepper from "../Stepper"
+import {GenesisStep, PhaseOne, PhaseThree, PhaseTwo, PhaseZero} from "./components/Steps"
+import "./index.scss"
 
-const Footer: FunctionComponent = () => (
-	<footer className="footer">
-		<section className="footer-heading">
-			<h1>Roadmap</h1>
-		</section>
-	</footer>
-)
+const Footer: FunctionComponent = () => {
+	const [currentStepIdx, setCurrentStepIdx] = useState(0)
+	const steps = [
+		{title: "Genesis", content: <GenesisStep />},
+		{title: "Phase 0", content: <PhaseZero />},
+		{title: "Phase 1", content: <PhaseOne />},
+		{title: "Phase 2", content: <PhaseTwo />},
+		{title: "Phase 3", content: <PhaseThree />}
+	]
+	const currentStep = steps[currentStepIdx]
+	return (
+		<footer className="footer">
+			<section className="footer__heading">
+				<h1>Roadmap</h1>
+				<Stepper currentStepIndex={currentStepIdx} steps={steps} onStepClick={setCurrentStepIdx} />
+			</section>
+			<section className="footer__content">{currentStep.content}</section>
+			<section className="footer__bottom">© Seker Factory 2022</section>
+		</footer>
+	)
+}
 
 export default Footer
