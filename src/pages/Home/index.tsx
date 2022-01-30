8cd2c90ba7d1d987f613661371af1b42445d4cd8
nginnever <ginneversource@gmail.com>
2022-01-30 15:52:06 -0800

    buidl the metaverse

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -135,7 +135,7 @@ const HomePage: FunctionComponent = () => {
 							Seker Factory is building an immersive tech noir of dystopian hangouts - the premier
 							spot to adventure from physical to digital. We are gathering like-minded individuals
 							to form a global community of digital artists, hackers, avatars, and futurists of all
-							kinds.
+							kinds to buidl the metaverse.
 						</h3>
 						<h3 className="purple">Come for the vibes. Stay for the revolution.</h3>
 						<h3>
