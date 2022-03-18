7744da963942b2b17e780efdc1fd483025d74845
nginnever <ginneversource@gmail.com>
2022-03-17 21:18:22 -0700

    copy update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -123,9 +123,9 @@ const HomePage: FunctionComponent = () => {
 					<section className="upcoming-events__about">
 						<h3>
 							Seker Factory is building an immersive tech noir of dystopian hangouts - the premier
-							spot to adventure from physical to digital. We are gathering like-minded individuals
-							to form a global community of digital artists, hackers, avatars, and futurists of all
-							kinds to navigate the metaverse.
+							spot to adventure from physical to digital. We are the architects of metaverse venues
+							and are gathering like-minded individuals to form a global community of digital
+							artists, hackers, avatars, and futurists of all kinds to navigate the metaverse.
 						</h3>
 						<h3 className="purple">Come for the vibes. Stay for the revolution.</h3>
 						<h3>
