f50c280d3b319cdb2aaaf418b7a112f2232ce89c
Kirill Klimenko <klimenkomud@gmail.com>
2022-04-26 21:02:22 +0300

    Finalize design

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,29 +1,35 @@
-import {FunctionComponent, useState} from "react"
-import Stepper from "../Stepper"
-import {GenesisStep, PhaseOne, PhaseThree, PhaseTwo} from "./components/Steps"
+import {FunctionComponent} from "react"
+import {Link} from "react-router-dom"
+import {ReactComponent as DiscordIcon} from "../../assets/icons/discord-grayscale.svg"
+import {ReactComponent as InstagramIcon} from "../../assets/icons/instagram_standard.svg"
+import {ReactComponent as MediumIcon} from "../../assets/icons/medium_standard.svg"
+import {ReactComponent as TwitterIcon} from "../../assets/icons/twitter-grayscale.svg"
+import {ReactComponent as YouTubeIcon} from "../../assets/icons/youtube_standard.svg"
 import "./index.scss"
 
-const Footer: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
-	const [currentStepIdx, setCurrentStepIdx] = useState(0)
-	const steps = [
-		{title: "Genesis", content: <GenesisStep />},
-		{title: "Phase 1", content: <PhaseOne />},
-		{title: "Phase 2", content: <PhaseTwo />},
-		{title: "Phase 3", content: <PhaseThree />}
-	]
-	const currentStep = steps[currentStepIdx]
-	return (
-		<footer className="footer">
-			<section className="footer__heading">
-				<h1>Roadmap</h1>
-				<Stepper currentStepIndex={currentStepIdx} steps={steps} onStepClick={setCurrentStepIdx} />
-			</section>
-			<section className="footer__content">{currentStep.content}</section>
-			<section className="footer__bottom">
-				<p>© Seker Factory 2022</p>
-			</section>
-		</footer>
-	)
-}
+const Footer: FunctionComponent<React.PropsWithChildren<unknown>> = () => (
+	<footer className="footer">
+		<section className="footer__left">
+			<p>© 2022 Seker Factory DAO LLC</p>
+		</section>
+		<section className="footer__right">
+			<Link to="https://discord.gg/rju5QnZmpM" target="_blank">
+				<DiscordIcon height="30px" width="26px" />
+			</Link>
+			<Link to="https://instagram.com/">
+				<InstagramIcon height="30px" width="30px" />
+			</Link>
+			<Link to="https://twitter.com/SekerFactory" target="_blank">
+				<TwitterIcon height="30px" width="37px" />
+			</Link>
+			<Link to="https://medium.com/SekerFactory" target="_blank">
+				<MediumIcon height="30px" width="30px" />
+			</Link>
+			<Link to="https://medium.com/SekerFactory" target="_blank">
+				<YouTubeIcon height="30px" width="30px" />
+			</Link>
+		</section>
+	</footer>
+)
 
 export default Footer
--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Header from "./components/Header"
 import {Web3Context} from "./context"
 import "./index.scss"
-import EventDetails from "./pages/EventDetails"
-import EventsList from "./pages/EventsList"
 import HomePage from "./pages/Home"
 
 const App: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
@@ -17,8 +15,6 @@ const App: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
 				<Header />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
-					<Route path="/events" element={<EventsList />} />
-					<Route path="/events/:id" element={<EventDetails />} />
 				</Routes>
 			</Router>
 		</Web3Context.Provider>
