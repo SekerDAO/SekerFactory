c4dcb43be20141d807e226ed3f3dd5cbdb248ecd
nginnever <ginneversource@gmail.com>
2022-10-08 13:37:04 -0700

    comment events out

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,7 @@ import SubscribeForm from "../../components/Subscribe"
 import About from "./components/About"
 import Membership from "./components/Membership"
 import Schedule from "./components/Schedule"
-import Slides from "./components/Slides"
+// import Slides from "./components/Slides"
 import "./index.scss"
 
 const HomePage: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
@@ -15,7 +15,7 @@ const HomePage: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
 		<>
 			<Schedule viewScheduleOpen={viewScheduleOpen} setViewScheduleOpen={setViewScheduleOpen} />
 			<main className="home-page">
-				<Slides />
+				{/*<Slides />*/}
 				<About />
 				<Membership />
 				<Grid container>
